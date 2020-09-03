ae9fc00e005c61855c909e6bc5aaa9f86261f769
Maarten <mvandevelde@xpertdoc.com>
2020-09-03 14:26:51 +0200

    availability + half hours not below zero

--- opvang/main.ts
+++ opvang/main.ts
@@ -1,13 +1,23 @@
 function doGet(e) {
-	// return error template if no id found
 	const sheetId = "16_SNwRVKHoWVevxmAzpJs-EmxUaEVavcaKaTn_vS_Qw";
 	const template = HtmlService.createTemplateFromFile('index');
 	const props = PropertiesService.getUserProperties();
+	const now = new Date();
+
+	// no userId found
 	if (!e.parameter || !e.parameter.userId) {
 		const errorTemplate = HtmlService.createTemplateFromFile('error');
 		return errorTemplate.evaluate()
 			.setTitle('Opvang');
 	}
+
+	// only available on weekdays between 6h and 19h
+	if (now.getDay() === 0 || now.getDay() === 6 || now.getHours() < 6 || now.getHours() > 19) {
+		const errorTemplate = HtmlService.createTemplateFromFile('unavailable');
+		return errorTemplate.evaluate()
+			.setTitle('Opvang');
+	}
+
 	const userId = e.parameter.userId;
 	const spreadsheet = SpreadsheetApp.openById(sheetId);
 
@@ -40,28 +50,30 @@ function formSubmit(formData) {
 	let property = props.getProperty("sheetId");
 	const spreadsheet = SpreadsheetApp.openById(property);
 	const timeSheet = spreadsheet.getSheetByName("Registraties");
-	const row = getCurrentRowForUser(timeSheet);
-	console.log("userId: " + userId + ", row: " + row);
+	const timeOfDay = now.getHours() >= 12 ? "A" : "O";
+	// const row = getCurrentRowForUser(timeSheet, userId, timeOfDay);
+	// always append, no overwrite
+	const row = timeSheet.getLastRow() + 1;
 
-	updateRow(timeSheet, row, userId, now, halfHours);
+	console.log("userId: " + userId + ", row: " + row);
+	updateRow(timeSheet, row, userId, now, halfHours, timeOfDay);
 	return {success: true, message: 'Sucessfully submitted!'};
 }
 
-function updateRow(sheet, row: number, userId: number, now: Date, halfHours: number) {
-	console.log(row);
+function updateRow(sheet, row: number, userId: number, now: Date, halfHours: number, timeOfDay: string) {
 	const target = sheet.getRange(row, 1, 1, 5);
-	target.setNumberFormat("HH:MM");
-	target.setValues([[userId, now, now.getHours() >= 12 ? "A" : "O", now, halfHours]]);
+	target.setValues([[userId, now, timeOfDay, now, halfHours]]);
 }
 
-function getCurrentRowForUser(sheet): number {
+function getCurrentRowForUser(sheet, userId, timeOfDay): number {
 	const lines = sheet.getRange(1, 1, sheet.getLastRow(), 3).getValues();
 	const today = new Date();
-	today.setHours(0, 0, 0, 0);
 	const row = lines.findIndex(value => {
-		const date = new Date(value);
-		return date === today;
+		const date = new Date(value[1]);
+		return value[0] == userId
+			&& date.getDay() === today.getDay() && date.getMonth() == today.getMonth() && date.getFullYear() == today.getFullYear()
+			&& value[2] == timeOfDay;
 	});
 
-	return row === -1 ? sheet.getLastRow() + 1 : row;
+	return row === -1 ? sheet.getLastRow() + 1 : row + 1;
 }
