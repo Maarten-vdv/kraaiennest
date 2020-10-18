4f3cd7bc08a0ad953cb0ff0153772b807e202377
Maarten <mvandevelde@xpertdoc.com>
2020-10-18 19:26:55 +0200

    update

--- opvang/main.ts
+++ opvang/main.ts
@@ -2,7 +2,7 @@ function doGet(e) {
 	const sheetId = "16_SNwRVKHoWVevxmAzpJs-EmxUaEVavcaKaTn_vS_Qw";
 	const template = HtmlService.createTemplateFromFile('index');
 	const props = PropertiesService.getUserProperties();
-	const now = new Date("2020-10-05T15:34:00");
+	const now = new Date();
 
 	const isPresence = e.parameter.mode === "presence";
 	const spreadsheet = SpreadsheetApp.openById(sheetId);
@@ -46,18 +46,47 @@ function getPresentNames() {
 	const timeOfDay = now.getHours() >= 12 ? 'A' : 'O'
 
 	const nameSheet = spreadsheet.getSheetByName("Namen");
-	const names = nameSheet.getRange(2, 1, nameSheet.getLastRow(), 2).getValues();
+	const names = nameSheet.getRange(2, 1, nameSheet.getLastRow(), 5).getValues();
 
 	const mainSheet = spreadsheet.getSheetByName("Registraties");
-	const registrations = mainSheet.getRange(2, 1, nameSheet.getLastRow(), 3).getValues().filter(row => {
+	let values = mainSheet.getRange(2, 1, mainSheet.getLastRow(), 4).getValues();
+	Logger.log(values.length);
+
+	const registrations = values.filter(row => {
 		const date = new Date(row[1]);
 		return date.getDate() === now.getDate() && date.getMonth() === now.getMonth() && row[2] === timeOfDay
 	} );
 
-	return registrations.map(r => {
-		const name = names.find(n => n[0] === r[0]);
-		return name ? name[1] : null
-	}).filter(n => !!n);
+	Logger.log(registrations);
+
+	const hash = registrations
+		.map(reg => ({name: names.find(name => name[0] === reg[0]), reg: reg}))
+		.reduce((acc, entry) => {
+			const array = acc[entry.name[1]];
+			const time = new Date(entry.reg[3]);
+			const timeString = time.getHours() + ':' + ('' + time.getMinutes()).padStart(2, '0');
+			acc[entry.name[1]] = array ? [...array, timeString] : [entry.name[4], entry.name[3], entry.name[2], timeString];
+			return acc;
+		}, {});
+
+
+	const regs = Object.keys(hash)
+		.filter(key => !!key)
+		.map(key => hash[key])
+		.sort((n1, n2) => {
+			return n1[0].localeCompare(n2[0]) || n1[1].localeCompare(n2[1]) || n1[2].localeCompare(n2[2])
+		});
+
+	const totals = regs.reduce((acc, r) => {
+		acc[r[0]] ? acc[r[0]]++ : acc[r[0]] = 1;
+		return acc;
+	}, {});
+	totals['totaal'] = regs.length;
+
+	return JSON.stringify({
+		totals: totals,
+		regs: regs
+	});
 }
 
 function getUser(spreadsheet, qrId) {
